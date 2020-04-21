0ab2a064442cce3b0be1af9c939b75e83d1a1e9f
fredy <frcontreras3@poligran.edu.co>
2020-04-21 15:59:02 -0500

    version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FilmsComponent } from './films/films.component';
+import { PeopleComponent } from './people/people.component';
+import { PlanetsComponent } from './planets/planets.Component';
+import { SpeciesComponent } from './species/species.component';
+import { StarshipsComponent } from './starships/starships.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'films', component: FilmsComponent },
+  { path: 'people', component: PeopleComponent },
+  { path: 'planets', component: PlanetsComponent },
+  { path: 'species', component: SpeciesComponent },
+  { path: 'starships', component: StarshipsComponent },
+  { path: 'vehicles', component: VehiclesComponent },
+  { path: '**', component: NoPageFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilmsComponent } from './films/films.component';
+import { PeopleComponent } from './people/people.component';
+import { PlanetsComponent } from './planets/planets.component';
+import { SpeciesComponent } from './species/species.component';
+import { StarshipsComponent } from './starships/starships.component';
+import { VehiclesComponent } from './vehicles/vehicles.component';
+import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmsComponent,
+    PeopleComponent,
+    PlanetsComponent,
+    SpeciesComponent,
+    StarshipsComponent,
+    VehiclesComponent,
+    NoPageFoundComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
